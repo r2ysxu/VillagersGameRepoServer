1931f6fc357bc86ea41b5566af33a22b8cbd651a
Arthur Xu <arthur.xu@nurun.com>
2016-04-20 10:08:10 -0400

    Maven dependencies added

---
+++ src/com/vgrs/config/SpringWebConfig.java
@@ -0,0 +1,28 @@
+package com.vgrs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({ "com.viewer.controller" })
+@Import({ SecurityConfig.class })
+public class SpringWebConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+}
