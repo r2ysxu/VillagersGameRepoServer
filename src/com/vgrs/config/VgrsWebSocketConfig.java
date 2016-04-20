d1d3b25c63d5cd3dcc1c712ec3bcd346089e0f6e
Arthur Xu <arthur.xu@nurun.com>
2016-04-20 10:26:12 -0400

    Added spring websocket config

---
+++ src/com/vgrs/config/VgrsWebSocketConfig.java
@@ -0,0 +1,19 @@
+package com.vgrs.config;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+public class VgrsWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/vgrs");
+	}
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/game1").withSockJS();
+	}
+}
